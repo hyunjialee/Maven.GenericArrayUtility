a92c5c06ae51e8f1988236e4fe2d35922c38b7a4
hyunjialee <hyjialee@gmail.com>
2023-11-21 22:42:18 -0500

    added methods and parameters

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,24 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+
+    public ArrayUtility (T[] placeHolder) {
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return 0;
+    }
+    public T getMostCommonFromMerge(T[] arrayToMerge){
+        return null;
+    }
+    public Integer getNumberOfOccurrences(T valueToRemove){
+        return 0;
+    }
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
+    public String toString() {
+        return null;
+    }
 }
