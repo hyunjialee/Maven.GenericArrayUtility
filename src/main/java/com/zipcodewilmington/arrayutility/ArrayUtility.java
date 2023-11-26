6b396dedb28eac0ccf401168ef098b57e1f00bcc
hyunjialee <hyjialee@gmail.com>
2023-11-25 19:38:57 -0500

    passed one test

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,17 +1,30 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T> {
     T[] inputArray;
 
+
     public ArrayUtility (T[] inputArray) {
         this.inputArray = inputArray;
     }
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
-        return 0;
+        int counter = 0;
+        List<T> newArray = new ArrayList<>(Arrays.asList(inputArray));
+        newArray.addAll(Arrays.asList(arrayToMerge));
+        for (int i = 0; i < newArray.size(); i++) {
+            if(newArray.get(i).equals(valueToEvaluate)) {
+                counter++;
+            }
+        }
+        return counter;
     }
     public T getMostCommonFromMerge(T[] arrayToMerge){
         return null;
